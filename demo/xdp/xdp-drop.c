c17f3adb6e02ebdbbbdc6bf456c9f58154eaf029
BinbinLee <lbbxsxlz@gmail.com>
2021-09-07 16:12:55 +0800

    add xdp demo

---
+++ demo/xdp/xdp-drop.c
@@ -0,0 +1,10 @@
+#include <linux/bpf.h>
+//#include <uapi/linux/bpf.h>
+#define SEC(NAME) __attribute__((section(NAME), used))
+
+SEC("xdp")
+int xdp_drop_the_world(struct xdp_md *ctx) {
+    return XDP_DROP;
+}
+
+char _license[] SEC("license") = "GPL";
