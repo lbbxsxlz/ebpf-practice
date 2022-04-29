264cc5731822c7ed76bdd31a83bf1742b88e4907
BinbinLee <lbbxsxlz@gmail.com>
2022-04-29 19:23:50 +0800

    update formats

--- helloBpf/hello_user.c
+++ helloBpf/hello_user.c
@@ -4,15 +4,15 @@
 
 int main(int argc, char **argv)
 {
-	char filename[256];
-	snprintf(filename, sizeof(filename), "%s_kern.o", argv[0]);
+    char filename[256];
+    snprintf(filename, sizeof(filename), "%s_kern.o", argv[0]);
 
-	if(load_bpf_file(filename) != 0)
- 	{
-  		printf("The kernel didn't load BPF program\n");
-  		return -1;
- 	}
+    if(load_bpf_file(filename) != 0)
+     {
+        printf("The kernel didn't load BPF program\n");
+        return -1;
+     }
 
-	read_trace_pipe();
-	return 0;
+    read_trace_pipe();
+    return 0;
 }
