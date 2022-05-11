fd1ced7d7444c57daf894de4b9fe45c520070941
BinbinLee <lbbxsxlz@gmail.com>
2022-05-11 14:42:29 +0800

    Create hello_bpfcc.py

---
+++ bcc/hello_bpfcc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# run in project examples directory with:
+# sudo python hello_bpfcc.py"
+
+from bcc import BPF
+
+# This may not work for 4.17 on x64, you need replace kprobe__sys_clone with kprobe____x64_sys_clone
+BPF(text='int kprobe__sys_clone(void *ctx) { bpf_trace_printk("Hello, World!\\n"); return 0; }').trace_print()
