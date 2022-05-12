7b164b370c76c0ba7e9f27676592e43b21401ae6
BinbinLee <lbbxsxlz@gmail.com>
2022-05-12 22:12:42 +0800

    Update hello_bpfcc.py

--- bcc/hello_bpfcc.py
+++ bcc/hello_bpfcc.py
@@ -5,12 +5,12 @@
 from bcc import BPF
 
 # This may not work for 4.17 on x64, you need replace kprobe__sys_clone with kprobe____x64_sys_clone
-Pro = '''
+prog = '''
 int kprobe__sys_clone(void *ctx)
 {
     bpf_trace_printk("Hello, World!\\n");
     return 0;
 }
 '''
-b = BPF(text=Pro)
+b = BPF(text=prog)
 b.trace_print()
