52c12925a7b95a385ea0a5e92f8cb450bca00f5b
BinbinLee <lbbxsxlz@gmail.com>
2022-05-13 19:12:31 +0800

    Create hello_tracepoint.py

---
+++ bcc/hello_tracepoint.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# sudo python3 ./hello_tracepoint.py
+# trace random reading
+from __future__ import print_function
+from bcc import BPF
+
+#load eBPF program
+b = BPF(text = '''
+        /*  TRACEPOINT_PROBE(random, urandon_read)  \
+             kernel tracepoint random:urandom_read. \
+             formats ref: /sys/kernel/debug/tracing/events/random/urandom_read/format
+        */
+        TRACEPOINT_PROBE(random, urandom_read) {
+                bpf_trace_printk("%d\\n", args->got_bits);
+
+                return 0;
+        }
+        ''')
+
+#header
+print("%-18s %-16s %-6s %s"%("TIME(s)", "COMM", "PID", "GOTBITS"))
+
+#format output
+while 1:
+    try:
+        (task, pid, cpu, flags, ts, msg) = b.trace_fields()
+    except ValueError:
+        continue
+    print("%-18.9f %-16s %-6d %s"%(ts, task, pid, msg))
+
